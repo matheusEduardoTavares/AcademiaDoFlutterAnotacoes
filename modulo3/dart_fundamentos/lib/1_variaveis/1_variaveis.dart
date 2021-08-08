3c4339a6b4ce90afb4a5e150703b746e577059ea
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-08-07 22:36:34 -0300

    Aula - Variaveis - Parte 1 - finalizada

---
+++ modulo3/dart_fundamentos/lib/1_variaveis/1_variaveis.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  ///int e double herdam de num
+  num qualquerNumero = 1;
+  num qualquerNumero2 = 1.2;
+
+  // int idade = 37;
+  var idade = 37;
+  ///double
+  var valorIphone = 12.000;
+
+  /// camelCase
+  var nomeCurso = 'Academia do Flutter';
+  var nomeMelhorCursoFlutter = 'Academia do Flutter';
+
+  /// SnackCase
+  var nome_curso = 'Academia do Flutter';
+  nome_curso = 'Academia';
+
+  ///String
+  var nomeCursoFlutter2;
+  nomeCursoFlutter2 = 'Academia do Flutter';
+
+  ///Todas classes no Dart herdam de [Object]
+  Object objetoQualquer = 1.2;
+  Object objetoQualquer2 = 1;
+  Object objetoQualquer3 = 'a';
+
+  dynamic dynamicQualquer = 1;
+  dynamicQualquer = 'Rodrigo';
+  dynamicQualquer = 1.2;
+}

5b1343edbc72f545b2fe6482d4ab6dfabd9c31cf
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-12-03 07:50:00 -0300

    Aula - Desafio - Pergunta

---
+++ modulo4/dart_funcoes_colecoes/lib/8_desafio/desafio.dart
@@ -0,0 +1,28 @@
+void main(List<String> args) {
+  //! Detalhe sobre a String
+  //! A String é composta por 3 campos (Nome|Idade|Sexo)
+  // ignore: unused_local_variable
+  final pessoas = [
+    'Rodrigo Rahman|35|Masculino',
+    'Jose|56|Masculino',
+    'Joaquim|84|Masculino',
+    'Rodrigo Rahman|35|Masculino',
+    'Maria|88|Feminino',
+    'Helena|24|Feminino',
+    'Leonardo|5|Masculino',
+    'Laura Maria|29|Feminino',
+    'Joaquim|72|Masculino',
+    'Helena|24|Feminino',
+    'Guilherme|15|Masculino',
+    'Manuela|85|Feminino',
+    'Leonardo|5|Masculino',
+    'Helena|24|Feminino',
+    'Laura|29|Feminino',
+  ];
+
+  //! Baseado na lista acima.
+  //! 1 - Remova os pacientes duplicados e apresente a nova lista
+  //! 2 - Me mostre a quantidade de pessoas por sexo (Masculino e Feminino) e depois me apresente o nome delas
+  //! 3 - Filtrar e deixar a lista somente com pessoas maiores de 18 anos e apresente essas pessoas pelo nome 
+  //! 4 - Encontre a pessoa mais velha e apresente o nome dela.
+}

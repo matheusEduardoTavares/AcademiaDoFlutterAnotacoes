9ff64b05f2ca6ba38f0e9821b7b484942dbeb644
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-12-29 09:19:13 -0300

    Aula - Entendendo como atualizar e voltar versao de banco de dados - finalizada

--- modulo15/flutter_sqlite_example/lib/database/database.dart
+++ modulo15/flutter_sqlite_example/lib/database/database.dart
@@ -12,7 +12,19 @@ class DatabaseSqlite {
 
     openDatabase(
       databaseFinalPath,
-      version: 1,
+      version: 2,
+      ///Executado sempre que abrir uma nova conexão, e
+      ///serve para exectar configurações que precisamos
+      ///fazer no banco de dados
+      onConfigure: (db) async {
+        debugPrint('onConfigure sendo chamado');
+
+        ///Esse comando habilita o uso de Foreign Keys para
+        ///o SQFlite
+        await db.execute('''
+          PRAGMA foreign_keys = ON
+        ''');
+      },
       ///Chamado somente no momento de criação do BD
       ///(primeira vez que carrega o APP)
       onCreate: (Database db, int version) {
@@ -26,15 +38,68 @@ class DatabaseSqlite {
           )
         ''');
 
+        batch.execute('''
+            create table produto(
+              id Integer primary key autoincrement,
+              nome varchar(200)
+            )
+          ''');
+
+        // batch.execute('''
+        //     create table categoria(
+        //       id Integer primary key autoincrement,
+        //       nome varchar(200)
+        //     )
+        //   ''');
+
         batch.commit();
       },
       ///Será chamado sempre que houver uma alteração no version incremetal
       onUpgrade: (Database db, int oldVersion, int version) {
         debugPrint('OnUpgrade chamado');
+
+        final batch = db.batch();
+
+        if (oldVersion == 1) {
+          batch.execute('''
+            create table produto(
+              id Integer primary key autoincrement,
+              nome varchar(200)
+            )
+          ''');
+
+          // batch.execute('''
+          //   create table categoria(
+          //     id Integer primary key autoincrement,
+          //     nome varchar(200)
+          //   )
+          // ''');
+        }
+
+        // if (oldVersion == 2) {
+        //   batch.execute('''
+        //     create table categoria(
+        //       id Integer primary key autoincrement,
+        //       nome varchar(200)
+        //     )
+        //   ''');
+        // }
+
+        batch.commit();
       },
       ///Será chamado sempre que houver uma versão no version decremental
       onDowngrade: (Database db, int oldVersion, int version) {
         debugPrint('onDOwngrade chamado');
+
+        final batch = db.batch();
+
+        if (oldVersion == 3) {
+          batch.execute('''
+            drop table categoria
+          ''');
+
+          batch.commit();
+        }
       },
     );
   }  

bde14c47c83b70c24d599d8f850a6e7e4646b41f
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-12-30 08:58:22 -0300

    Aula - Criando projeto e configurando arquitetura de pastas - finalizada

---
+++ modulo17/todo_list_provider/lib/app/app_module.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/app_widget.dart';
+
+///Tudo que for compartilhado por toda a
+///aplicação ficará dentro deste AppModule,
+///é o módulo core do sistema, fica todas as 
+///classes compartilhada por vários módulos,
+///ficará aqui as dependências que ficam
+///na memória todo o tempo enquanto o APP está
+///rodando. Cada módulo específico terá também
+///dependências específicas que só ficarão ativas 
+///quando aquele módulo estiver ativo
+class AppModule extends StatelessWidget {
+  const AppModule({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => Object()),
+      ],
+      child: AppWidget(),
+    );
+  }
+}

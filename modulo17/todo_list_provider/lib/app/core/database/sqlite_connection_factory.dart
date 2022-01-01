e625b0a3c3c34d3d0d6f2e09e617630344625062
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-12-31 23:26:20 -0300

    Aula - Configurando SqliteConnection SqliteADM e Migrations - Parte 1 - finalizada

---
+++ modulo17/todo_list_provider/lib/app/core/database/sqlite_connection_factory.dart
@@ -0,0 +1,60 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:synchronized/synchronized.dart';
+
+class SqliteConnectionFactory {
+  static SqliteConnectionFactory? _instance;
+
+  SqliteConnectionFactory._();
+
+  factory SqliteConnectionFactory() {
+    return _instance ??= SqliteConnectionFactory._();
+  }
+
+  static const _VERSION = 1;
+  static const _DATABASE_NAME = 'TODO_LIST_PROVIDER';
+
+  Database? _db;
+  final _lock = Lock();
+
+  Future<Database> openConnection() async {
+    var databasePath = await getDatabasesPath();
+    var databasePathFinal = join(databasePath, _DATABASE_NAME);
+
+    if (_db == null) {
+      await _lock.synchronized(() async {
+        _db ??= await openDatabase(
+          databasePathFinal,
+          version: _VERSION,
+          onConfigure: _onConfigure,
+          onCreate: _onCreate,
+          onDowngrade: _onDowngrade,
+          onUpgrade: _onUpgrade,
+        );
+      });
+    }
+
+    return _db!;
+  }
+
+  void closeConnection() {
+    _db?.close();
+    _db = null;
+  }
+
+  Future<void> _onConfigure(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+
+  }
+
+  Future<void> _onUpgrade(Database db, int oldVersion, int version) async {
+
+  }
+
+  Future<void> _onDowngrade(Database db, int oldVersion, int version) async {
+
+  }
+}

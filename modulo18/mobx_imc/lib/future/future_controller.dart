3892bef5023ae7f1f054150b724d77fb8c31d346
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2022-01-07 20:58:50 -0300

    Aula - ObservableFuture - Parte 2 - finalizada

--- modulo18/mobx_imc/lib/future/future_controller.dart
+++ modulo18/mobx_imc/lib/future/future_controller.dart
@@ -6,12 +6,25 @@ class FutureController = _FutureControllerBase with _$FutureController;
 abstract class _FutureControllerBase with Store {
   @observable 
   // var nomeFuture = ObservableFuture(Future.value());
-  var nomeFuture = Future.value('').asObservable();
+  // var nomeFuture = Future.value('').asObservable();
+  var nomeFuture = ObservableFuture.value('');
 
   @action
   Future<void> buscarNome() async {
-    nomeFuture = Future.delayed(Duration(seconds: 2), () {
+    ///Das 2 formas funcionam:
+    
+    // nomeFuture = Future.delayed(Duration(seconds: 2), () {
+    //   return 'Rodrigo Rahman';
+    // }).asObservable();
+
+    final buscaNomeFuture = Future.delayed(Duration(seconds: 2), () {
       return 'Rodrigo Rahman';
-    }).asObservable();
+    });
+
+    // nomeFuture = buscaNomeFuture.asObservable();
+    nomeFuture = ObservableFuture(buscaNomeFuture);
+
+    ///Se quiser esperar ele terminar, poderíamos fazer isso:
+    // await buscaNomeFuture;
   }
 }

caba39d9d11512323a5f97ef5d2b565b1dd328ab
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2022-01-07 21:57:17 -0300

    Aula - Fundamento e configuracoes - finalizada

---
+++ modulo19/flutter_modular_example/lib/app/app_module.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  
+}
---
+++ modulo19/flutter_modular_example/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular_example/app/app_module.dart';
+import 'package:flutter_modular_example/app/app_widget.dart';
+
+void main() {
+  runApp(ModularApp(
+    module: AppModule(),
+    child: AppWidget(),
+  ));
+}

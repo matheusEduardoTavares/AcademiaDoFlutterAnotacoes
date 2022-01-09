83d6caebaf867441f835bb9df28ce3ad2bc38b6e
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2022-01-09 19:45:08 -0300

    Aula - Atualizando packages e acertos de codigo - finalizada

--- modulo20/cuidapet_mobile/lib/app/core/ui/extensions/theme_extension.dart
+++ modulo20/cuidapet_mobile/lib/app/core/ui/extensions/theme_extension.dart
@@ -4,8 +4,5 @@ extension ThemeExtension on BuildContext {
   Color get primaryColor => Theme.of(this).primaryColor;
   Color get primaryColorDark => Theme.of(this).primaryColorDark;
   Color get primaryColorLight => Theme.of(this).primaryColorLight;
-  // TODO! Verify theme update
-  // ignore: deprecated_member_use
-  Color get accentColor => Theme.of(this).accentColor;
   TextTheme get textTheme => Theme.of(this).textTheme;
 }
--- modulo20/cuidapet_mobile/lib/app/core/ui/ui_config.dart
+++ modulo20/cuidapet_mobile/lib/app/core/ui/ui_config.dart
@@ -9,7 +9,5 @@ class UiConfig {
     primaryColor: const Color(0xFFA8CE48),
     primaryColorDark: const Color(0xFF689F38),
     primaryColorLight: const Color(0xFFDDEDC7),
-    // ignore: deprecated_member_use
-    accentColor: const Color(0xFF4CAF50),
   );
 }

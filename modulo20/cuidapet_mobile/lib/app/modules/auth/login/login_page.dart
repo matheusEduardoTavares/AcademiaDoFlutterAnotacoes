fcfda5c7cfe31b9f1f079feab3d41b741abd6d8f
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2022-01-14 20:10:19 -0300

    Aula - Encapsulando log aplicacao - finalizada

--- modulo20/cuidapet_mobile/lib/app/modules/auth/login/login_page.dart
+++ modulo20/cuidapet_mobile/lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:cuidapet_mobile/app/core/helpers/environments.dart';
+import 'package:cuidapet_mobile/app/core/helpers/logger.dart';
 import 'package:cuidapet_mobile/app/core/ui/extensions/size_screen_extension.dart';
 import 'package:cuidapet_mobile/app/core/ui/extensions/theme_extension.dart';
 import 'package:cuidapet_mobile/app/modules/auth/login/widgets/login_form.dart';
--- modulo20/cuidapet_mobile/lib/app/modules/core/core_module.dart
+++ modulo20/cuidapet_mobile/lib/app/modules/core/core_module.dart
@@ -1,3 +1,4 @@
+import 'package:cuidapet_mobile/app/core/helpers/logger.dart';
 import 'package:cuidapet_mobile/app/core/rest_client/dio_rest_client.dart';
 import 'package:cuidapet_mobile/app/core/rest_client/rest_client.dart';
 import 'package:cuidapet_mobile/app/modules/core/auth/auth_store.dart';
@@ -8,5 +9,6 @@ class CoreModule extends Module {
   final List<Bind> binds = [
     Bind.lazySingleton((i) => AuthStore(), export: true),
     Bind.factory<RestClient>((i) => DioRestClient(), export: true),
+    Bind.lazySingleton<Logger>((i) => LoggerImpl(), export: true),
   ];
 }

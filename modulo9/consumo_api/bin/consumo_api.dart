6c610cda51e18d13f79c6e61b118f804978a365a
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2022-04-23 20:11:23 -0300

    Aula - Trabalhando com JSON no Dart - finalizada

--- modulo9/consumo_api/bin/consumo_api.dart
+++ modulo9/consumo_api/bin/consumo_api.dart
@@ -1,5 +1,48 @@
-import 'package:consumo_api/consumo_api.dart' as consumo_api;
+import 'dart:convert';
 
 void main(List<String> arguments) {
-  print('Hello world: ${consumo_api.calculate()}!');
+  final cidadeJson = '''
+    [
+      {
+        "id": 1,
+        "nome": "Santo André",
+        "regiao": {
+          "nome": "ABC Paulista"
+        }
+      },
+      {
+        "id": 2,
+        "nome": "São Paulo",
+        "regiao": {
+          "nome": "ABC Paulista"
+        }
+      }
+    ]
+  ''';
+
+  // [] -> List
+  // {} -> Map<String, dynamic>
+  final cidadeMap = jsonDecode(cidadeJson);
+
+  if (cidadeMap is List) {
+    print('Uma lista');
+    cidadeMap.forEach((city) {
+      print('nome: ${city['nome']} | Região: ${city['regiao']['nome']}');
+    });
+  }
+  else if (cidadeMap is Map) {
+    print('Um map');
+    print(cidadeMap['nome']);
+  }
+
+  print(cidadeMap);
+  print(cidadeMap.runtimeType);
+
+  final cidadeMapJson = <String, dynamic>{
+    'id': 123,
+    'nome': 'Santo André',
+    'uf': 'SP'
+  };
+
+  print(jsonEncode([cidadeMapJson]));
 }

5e1a932e3802f8d6b5a10f715d9a32b2258e8505
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2022-04-28 04:31:13 -0300

    Aula - Criando nosso primeiro bloc - Parte 2 - finalizada

--- modulo20/counter_bloc/lib/home_page.dart
+++ modulo20/counter_bloc/lib/home_page.dart
@@ -23,7 +23,9 @@ class HomePage extends StatelessWidget {
               ),
             ),
             TextButton(
-              onPressed: () {}, 
+              onPressed: () {
+                Navigator.of(context).pushNamed('/cubit');
+              }, 
               child: Text(
                 'Cubit',
                 style: Theme.of(context).textTheme.headlineMedium,
--- modulo20/counter_bloc/lib/main.dart
+++ modulo20/counter_bloc/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:counter_bloc/bloc/counter_bloc.dart';
 import 'package:counter_bloc/home_page.dart';
 import 'package:counter_bloc/page_bloc/counter_bloc_page.dart';
 import 'package:counter_bloc/page_cubit/counter_cubit_page.dart';
+import 'package:counter_bloc/page_cubit/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -24,7 +25,10 @@ class MyApp extends StatelessWidget {
           create: (_) => CounterBloc(),
           child: const CounterBlocPage(),
         ),
-        '/cubit': (_) => const CounterCubitPage()
+        '/cubit': (_) => BlocProvider(
+          create: (_) => CounterCubit(),
+          child: const CounterCubitPage(),
+        )
       },
       home: const HomePage(),
     );

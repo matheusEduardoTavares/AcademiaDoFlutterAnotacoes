c1583aa4f56d4a1de8d1ab2877b69aea508581ed
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2022-05-01 03:52:59 -0300

    Aula - Entendendo outros widgets do bloc - Parte 1 - finalizada

--- modulo20/contact_bloc/lib/features/bloc_example/bloc_example.dart
+++ modulo20/contact_bloc/lib/features/bloc_example/bloc_example.dart
@@ -11,19 +11,86 @@ class BlocExample extends StatelessWidget {
       appBar: AppBar(
         title: const Text('BlocExample'),
       ),
-      body: BlocBuilder<ExampleBloc, ExampleState>(
-        builder: (_, state) {
-          if (state is ExampleStateData) {
-            return ListView.builder(
-              itemCount: state.names.length,
-              itemBuilder: (_, index) => ListTile(
-                title: Text(state.names[index]),
-              )
-            );
-          }
+      body: Column(
+        children: [
+          BlocSelector<ExampleBloc, ExampleState, bool>(
+            selector: (state) {
+              if (state is ExampleStateInitial) {
+                return true;
+              }
 
-          return const SizedBox.shrink();
-        },
+              return false;
+            }, 
+            builder: (_, showLoader) {
+              if (showLoader) {
+                return const Expanded(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              }
+
+              return const SizedBox.shrink();
+            }
+          ),
+          BlocConsumer<ExampleBloc, ExampleState>(
+            builder: (_, state) {
+              if (state is ExampleStateData) {
+                return Text('Total de nomes é ${state.names.length}');
+              }
+
+              return const SizedBox.shrink();
+            }, 
+            listener: (_, state) {
+              debugPrint('Estado alterado para ${state.runtimeType}');
+            },
+          ),
+          BlocListener<ExampleBloc, ExampleState>(
+            listener: (_, state) {
+              if (state is ExampleStateData) {
+                ScaffoldMessenger.of(context)
+                  .showSnackBar(
+                    SnackBar(
+                      content: Text('A quantidade de nomes é ${state.names.length}')
+                    )
+                  );
+              }
+            },
+            child: BlocSelector<ExampleBloc, ExampleState, List<String>>(
+              builder: (_, items) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: items.length,
+                  itemBuilder: (_, index) => ListTile(
+                    title: Text(items[index]),
+                  )
+                );
+              },
+              selector: (state) {
+                if (state is ExampleStateData) {
+                  return state.names;
+                }
+
+                return [];
+              },
+            ),
+            // child: BlocBuilder<ExampleBloc, ExampleState>(
+            //   builder: (_, state) {
+            //     if (state is ExampleStateData) {
+            //       return ListView.builder(
+            //         shrinkWrap: true,
+            //         itemCount: state.names.length,
+            //         itemBuilder: (_, index) => ListTile(
+            //           title: Text(state.names[index]),
+            //         )
+            //       );
+            //     }
+
+            //     return const SizedBox.shrink();
+            //   },
+            // ),
+          ),
+        ],
       ),
     );
   }

0edb4f2c9f05bce9dfb7e0600c34182ce8d04c7e
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2022-06-20 23:04:32 -0300

    Aula - Desafio - finalizada

--- modulo20/contact_bloc/test/features/contacts_cubit/list/cubit/contact_list_cubit_test.dart
+++ modulo20/contact_bloc/test/features/contacts_cubit/list/cubit/contact_list_cubit_test.dart
@@ -12,6 +12,11 @@ void main() {
   late ContactsRepository repository;
   late ContactListCubit bloc;
   late List<ContactModel> contacts;
+  final mockContact = ContactModel(
+    id: '1',
+    name: 'Rodrigo Rahman', 
+    email: 'rodrigorahman@academiadoflutter.com.br'
+  );
 
   // preparação
   setUp(() {
@@ -20,11 +25,9 @@ void main() {
       repository: repository,
     );
     contacts = [
+      mockContact,
       ContactModel(
-        name: 'Rodrigo Rahman', 
-        email: 'rodrigorahman@academiadoflutter.com.br'
-      ),
-      ContactModel(
+        id: '2',
         name: 'Rodrigo Rahman Pessoal', 
         email: 'rodrigorahman@gmail.com'
       ),
@@ -53,5 +56,48 @@ void main() {
       ContactListCubitState.loading(),
       ContactListCubitState.error(error: 'Erro ao buscar contatos'),
     ],
+  );
+
+  blocTest<ContactListCubit, ContactListCubitState>(
+    'Deve deletar um contato corretamente',
+    build: () => bloc,
+    act: (bloc) => bloc.deleteByModel(mockContact),
+    expect: () => [
+      ContactListCubitState.loading(),
+      ContactListCubitState.data(contacts: contacts),
+    ],
+    verify: (cubit) {
+      expect(cubit.state.maybeWhen(
+        orElse: () => false,
+        data: (contacts) => true,
+      ), true);
+      expect(cubit.state.maybeWhen(
+        orElse: () => <ContactModel>[],
+        data: (contacts) => contacts,
+      ), hasLength(1));
+    },
+    setUp: () {
+      when(() => repository.delete(mockContact)).thenAnswer((_) async {});
+      when(() => repository.findAll()).thenAnswer((_) async => contacts..removeWhere((element) => element == mockContact));
+    }
+  );
+
+  blocTest<ContactListCubit, ContactListCubitState>(
+    'Deve dar erro ao deletar um contato',
+    build: () => bloc,
+    act: (bloc) => bloc.deleteByModel(mockContact),
+    expect: () => [
+      ContactListCubitState.loading(),
+      ContactListCubitState.error(error: 'Erro ao buscar contatos'),
+    ],
+    verify: (cubit) {
+      expect(cubit.state.maybeWhen(
+        orElse: () => false,
+        error: (message) => true,
+      ), true);
+    },
+    setUp: () {
+      when(() => repository.delete(mockContact)).thenAnswer((_) async {});
+    }
   );
 }
